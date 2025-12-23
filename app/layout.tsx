cb59422c849d102d4af3cc147e662cc7deb407db
pedro noronha <myserywork@gmail.com>
2025-12-23 19:27:40 -0300

    Fix Meta Pixel: move Script to body with dangerouslySetInnerHTML for proper execution

--- app/layout.tsx
+++ app/layout.tsx
@@ -154,23 +154,7 @@ export default function RootLayout({
           `}
         </Script>
         
-        {/* Meta Pixel - Using dangerouslySetInnerHTML for proper execution */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1194474569538928');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
+        {/* Meta Pixel noscript fallback */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1194474569538928&ev=PageView&noscript=1" />`
@@ -208,6 +192,25 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-sans antialiased`}>
+        {/* Meta Pixel - Must be in body for Next.js */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1194474569538928');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         <div className="min-h-screen animated-bg noise-overlay">
           {children}
         </div>
