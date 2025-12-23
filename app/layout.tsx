3e5c499e4b816ba1a0148fd983539829b0c0eee3
pedro noronha <myserywork@gmail.com>
2025-12-23 19:21:21 -0300

    Fix Meta Pixel: use native script tag with dangerouslySetInnerHTML for proper event firing

--- app/layout.tsx
+++ app/layout.tsx
@@ -154,21 +154,23 @@ export default function RootLayout({
           `}
         </Script>
         
-        {/* Meta Pixel - Load early */}
-        <Script id="fb-pixel" strategy="beforeInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1194474569538928');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* Meta Pixel - Using dangerouslySetInnerHTML for proper execution */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1194474569538928');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1194474569538928&ev=PageView&noscript=1" />`
